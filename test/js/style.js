535d858b4be0840281abfcf4a1351d14ba0b8c6f
Liweihan55 <412387910@qq.com>
2015-09-20 12:16:11 +0800

    test

--- test/js/style.js
+++ test/js/style.js
@@ -26,18 +26,46 @@ window.onload = function () {
 			var userDd = document.createElement('dd');
 			userDl.appendChild(userDd);
 			userDd.className = "userDd";
+			//用户
 			var userNameText = document.createTextNode(userNameInp.value);
 			var userName = document.createElement('a');
 			userName.className = "userName";
 			userName.appendChild(userNameText);
 			userName.href = "###";
 			userDd.appendChild(userName);
-
+			//内容
 			var userPtext = document.createTextNode(textarea.value);
 			var userP = document.createElement('p');
 			userP.className = "userP";
 			userP.appendChild(userPtext);
 			userDd.appendChild(userP);
+			//时间
+			var time = new Date();
+			var str_T = "";
+			str_T += time.getFullYear() + "年";
+			str_T += time.getMonth()+1 + "月";
+			str_T += time.getDay() + "日 ";
+			str_T += time.getHours() + ":";
+			str_T += time.getMinutes();
+			var userTtext = document.createTextNode(str_T);
+			var userT = document.createElement('p');
+			userT.className = "userT"
+			userT.appendChild(userTtext);
+			//删除按钮
+			var remove = document.createElement('a');
+			remove.className = "remove";
+			remove.href = "###";
+			remove.innerHTML = "删除";
+			remove.onclick = function () {
+				var mymessage =confirm("确定删除？");
+				if(mymessage == true){
+					context.removeChild(this.parentNode.parentNode.parentNode);
+				}
+			}
+
+			userT.appendChild(remove);
+			userDd.appendChild(userT);
+
 
 			textarea.value = "";	
 			userNameInp.value = "";
